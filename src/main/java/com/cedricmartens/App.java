987a312dcba640b5c607c43cd48ef1aef7019ec8
MartensCedric <cedricmartens98@gmail.com>
2018-01-09 11:25:51 -0500

    Added error clear error messages for common mistakes

--- src/main/java/com/cedricmartens/App.java
+++ src/main/java/com/cedricmartens/App.java
@@ -9,6 +9,7 @@ public class App
         if(args.length != 0)
         {
             String filename = args[0];
+            String currency = "btc";
             BufferedReader br = null;
             FileReader fr = null;
             String[] data;
@@ -17,9 +18,22 @@ public class App
                 fr = new FileReader(filename);
                 br = new BufferedReader(fr);
                 data = br.readLine().split(",");
-                new TrustlessGenerator<String>(data).generate();
-            }catch (Exception e)
-            {
+
+                if(br.readLine() == null)
+                {
+                    try{
+                        new TrustlessGenerator<String>(currency,data).generate();
+                    }catch (FileNotFoundException e)
+                    {
+                        System.err.println("Failed to read information from the " + currency + " blockchain at https://api.blockcypher.com/v1/" + currency + "/main");
+                    }
+                }else{
+                    System.err.println("File not supported.\nMake sure the file separates values with commas and does not include newline characters.");
+                }
+
+            } catch (FileNotFoundException e) {
+                System.err.println("File '" + filename + "' not found");
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }else{
